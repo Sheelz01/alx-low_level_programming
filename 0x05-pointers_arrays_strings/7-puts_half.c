f5bf563626ba512d4c9a95eeee083f5a3877af80
Sheelz01 <isheenagh@yahoo.com>
2022-09-19 21:17:00 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -14,6 +14,7 @@ void puts_half(char *str)
 	
 	if(l % 2 == 0) {
 		for (n = 1 / 2; str [n] != '\0'; n++)
+			_putchar(str[n]);
 		} else {
 			for (n = ((l - 1) / 2) + 1; str[n] != '\0'; n++)
 				_putchar(str[n]);
