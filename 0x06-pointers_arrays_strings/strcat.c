52d70cc6b7d0edffc14fc618080589e673862ea5
Sheelz01 <isheenagh@yahoo.com>
2022-09-22 12:03:30 -0700

    strcat

--- 0x06-pointers_arrays_strings/strcat.c
+++ 0x06-pointers_arrays_strings/strcat.c
@@ -3,10 +3,10 @@
 /**
 *_strcat - concatenates the string pointed to by @src to
 *the end of the string pointed to by @dest
-*@dest: String that will be appended
+*@dest:String that will be appended
 *@src: String to be concatenated upon
 *
-*Return: return poiner to @dest
+*Return: returns pointer to @dest
 */
 
 char *_strcat(char *dest, char *src)
@@ -14,18 +14,11 @@ char *_strcat(char *dest, char *src)
 
 	int index = 0, dest_len = 0;
 
-	while (dest[index++])
-		dest_len++;
+		while (dest[index++])
+			dest_len++;
 
 	for (index = 0; src[index]; index++)
 		dest[dest_len++] = src[index];
 
 	return (dest);
 }
-
-
-
-
-
-
-
