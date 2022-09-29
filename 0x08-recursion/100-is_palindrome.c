5ac1492a3e8cc9ff721bfff9b409395995f3474d
Sheelz01 <isheenagh@yahoo.com>
2022-09-29 16:38:50 -0700

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -15,6 +15,27 @@ int is_palindrome(char *s)
 }
 
 /**
+ * check - checks if a string is a palindrome
+ * @s: string to be checkedv
+ * @start: start index
+ * @end: end index
+ * @flag: flag to indicate if a string is a palindrome
+ *
+ * Return: void
+ */
+void check(char *s, int start, int end, int *flag)
+{
+	if (start <= end)
+	{
+		if (s[start] == s[end])
+			*flag *= 1;
+		else
+			*flag *= 0;
+		check(s, start + 1, end - 1, flag);
+	}
+}
+
+/**
  * _strlen_recursion - calculates the length of a string
  * @s: string to be used
  *
