6acd5418e0abe3914090888d37dc741661dc38af
Sheelz01 <isheenagh@yahoo.com>
2022-09-30 15:12:49 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,12 +2,13 @@
 #include "main.h"
 
 /**
- * main-print the name of the program
+ * main - print the name of the program
  * @argc: Count arguments
  * @argv: Arguments
  *
  * Return: Always 0 (Success)
  */
+
 int main(int argc, char *argv[])
 {
 	(void) argv; /*Ignore argv*/
