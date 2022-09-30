923d8867a91448d65a55db4e770fbc5e32c40f19
Sheelz01 <isheenagh@yahoo.com>
2022-09-30 08:27:15 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,12 +8,11 @@
  *
  * Return: Always 0 (Success) 
  */
-
 int main(int argc, char *argv[]) 
 {
-(void) argc; 
-printf("%s\n", argv[0]);
+	(void) argc; 
+	printf("%s\n", argv[0]);
 
-return (0);
+	return (0);
 }
 
